f156ab19de8de25bf12f67451a49a68d4944cd2a
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-05 15:06:34 +0200

    Change file for styling

--- laravel-and-beyond/resources/views/layouts/layout.blade.php
+++ laravel-and-beyond/resources/views/layouts/layout.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="app.css">
 </head>
 
 <body>
     <main>
-        @include('layouts.style')
         @include('_partials.header')
         @yield('home')
         @include('_partials.footer')
