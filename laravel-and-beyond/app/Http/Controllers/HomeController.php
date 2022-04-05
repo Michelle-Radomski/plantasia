d5b6ea39900fde4d4a615cfef5970ea0b83f0bba
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-05 14:32:07 +0200

    Create, get and show data

--- laravel-and-beyond/app/Http/Controllers/HomeController.php
+++ laravel-and-beyond/app/Http/Controllers/HomeController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Plant;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        return view('home');
+        $plant = Plant::find(1);
+        return view('home', ['plant' => $plant]);
     }
 }
--- laravel-and-beyond/resources/views/home.blade.php
+++ laravel-and-beyond/resources/views/home.blade.php
@@ -4,4 +4,6 @@
     <h1>Would you like to have an overview of your green friends?</h1>
     <p>This website will help you keep track of you collection of plants. You can easely acces your overview <a
             href="#">here</a></p>
-@endsection
+    <p>{{ $plant->nickname }} is a nice plant
+    <p>
+    @endsection
