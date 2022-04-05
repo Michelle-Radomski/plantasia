fa73bb8891d13b92d3a93b8abc165336fbe85776
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-05 09:59:39 +0200

    Change project name

---
+++ laravel-and-beyond/resources/views/_partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; Michelle Radomski</p>
+</footer>
---
+++ laravel-and-beyond/resources/views/_partials/header.blade.php
@@ -0,0 +1,9 @@
+<header>
+    <nav>
+        <ul>
+            <li id="name">Plantasia</li>
+            <li><a href="#">My Plants</a></li>
+            <li class="right"><a href="#">Repot</a></li>
+        </ul>
+    </nav>
+</header>
