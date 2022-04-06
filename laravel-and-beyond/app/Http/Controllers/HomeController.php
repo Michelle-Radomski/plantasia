939157cbc5c73191b6eeaa7a8a20a0e437b23502
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-06 15:02:10 +0200

    Get info from db and put in function

--- laravel-and-beyond/app/Http/Controllers/HomeController.php
+++ laravel-and-beyond/app/Http/Controllers/HomeController.php
@@ -4,17 +4,28 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Plant;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function home()
     {
-        $plant = Plant::find(1);
-        return view('home', ['plant' => $plant]);
+        return view('home');
     }
 
     public function plantsOverview()
     {
-        return view('myplants');
+        // $plant = Plant::find(1);
+        $user = User::find(1);
+        $plants = Plant::all();
+        return view('myplants', ['plants' => $plants, 'user' => $user]);
+    }
+
+    //TODO: CREATE DETAIL PAGE FOR SPECIFIC PLANT
+    public function plantDetails(Request $request)
+    {
+        $id = $request->id;
+        $plant = Plant::find($id);
+        return view('plantdetails', ['plant' => $plant]);
     }
 }
