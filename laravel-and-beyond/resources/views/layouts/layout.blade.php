d24cb6bb0ef021e92e52b9cadf7c8c457d225297
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-06 15:08:40 +0200

    Correct css link + add view in layout

--- laravel-and-beyond/resources/views/layouts/layout.blade.php
+++ laravel-and-beyond/resources/views/layouts/layout.blade.php
@@ -5,17 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="app.css">
+    <title>Plantasia</title>
+    <link rel="stylesheet" href="{{ asset('app.css') }}">
 </head>
 
 <body>
+    @include('_partials.header')
     <main>
-        @include('_partials.header')
         @yield('home')
         @yield('myplants')
-        @include('_partials.footer')
+        @yield('plantdetails')
     </main>
+    @include('_partials.footer')
 </body>
 
 </html>
