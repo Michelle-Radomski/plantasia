fcf4a1e9e86f92afbdfd626e5e73f3b23172290a
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-07 14:25:09 +0200

    Add text for homepage

--- laravel-and-beyond/resources/views/home.blade.php
+++ laravel-and-beyond/resources/views/home.blade.php
@@ -1,7 +1,42 @@
 @extends('layouts.layout')
 
 @section('home')
-    <h1>Would you like to have an overview of your green friends?</h1>
-    <p>This website will help you keep track of you collection of plants. You can easely acces your overview <a
-            href="myplants">here</a></p>
+    <section class="plantasiahomepagecontent">
+        <h1>Plantasia, keep track of your urban jungle</h1>
+        <h2>Overview for your green friends</h2>
+        <p>Sometimes we don't see the wood for the trees anymore. We love plants so much that we buy them every chance
+            we
+            get.
+            Before you know it you have your own urban jungle in your living room! It can be quite challenging to take
+            care
+            of our little babies in that case.
+            Luckily Plantasia has you covered! It will help you keep track of your collection of plants. You can easily
+            access your overview <a href="myplants">here</a> or via My Plants.
+        </p>
+        <h2>What else has Plantasia to offer?</h2>
+        <p>Not only can you check information about your plants, you can also add, update and even delete (RIP) plants
+            in your collection.
+            Isn't that neat?!
+        </p>
+        <h3>Add</h3>
+        <p>So, you could not resist and bought a new plant? Time to add it to your collection!
+            You can add plants in the My Plants section where you can also find an overview of your plants.
+        </p>
+        <h3>Update</h3>
+        <p>You're not happy with the name you gave your plant, you were so excited that you miss typed some info or
+            you found out something interesting about your little buddy? Whatever the reason,
+            it's always handy to be able to update information about a specific plant.
+            You've probably noticed the big button "Update" when you visit a specific plant in your collection?
+        </p>
+        <h3>Delete</h3>
+        <p>Ofourse we do everything we can to make our plants feel at home but sometimes things go wrong and they die.
+            Or,
+            on a more positive note,
+            maybe you decided to give away a plant as a gift! To keep our collection clean and tidy, it's necesarry to
+            delete some plants from our colelction.
+            As for anything else, we got you covered. You can delete any specific plant that's no longer in your
+            possession.
+            Just like with update you can find a "Delete" button when you visit a specific plant in your collection.
+        </p>
+    </section>
 @endsection
