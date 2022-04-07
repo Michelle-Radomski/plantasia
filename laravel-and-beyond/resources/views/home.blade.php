a244142fff187f836a07f85ff5e2d9bf976f0c48
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-07 09:24:47 +0200

    Delete some test code

--- laravel-and-beyond/resources/views/home.blade.php
+++ laravel-and-beyond/resources/views/home.blade.php
@@ -4,6 +4,4 @@
     <h1>Would you like to have an overview of your green friends?</h1>
     <p>This website will help you keep track of you collection of plants. You can easely acces your overview <a
             href="myplants">here</a></p>
-    <p>{{ $plant->nickname }} is a nice plant
-    <p>
-    @endsection
+@endsection
