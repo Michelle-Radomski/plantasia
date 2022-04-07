aa169fbf1b25a3cc32e44b59bb156e3cc51c3a08
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-07 11:22:12 +0200

    Add classes for styling

--- laravel-and-beyond/resources/views/myplants.blade.php
+++ laravel-and-beyond/resources/views/myplants.blade.php
@@ -3,11 +3,13 @@
 @section('myplants')
     <p>Welcome {{ $user->name }}!</p>
     <p>Here is an overview of your plants:</p>
-    @foreach ($plants as $plant)
-        <article>
-            <h1>{{ $plant->nickname }}</h1>
-            <p>{{ $plant->official_name }}</p>
-            <a href="myplants/{{ $plant->id }}">More info</a>
-        </article>
-    @endforeach
+    <section class="plantoverviewcontainer">
+        @foreach ($plants as $plant)
+            <article class="plantoverviewsmall">
+                <h1>{{ $plant->nickname }}</h1>
+                <p>{{ $plant->official_name }}</p>
+                <a href="myplants/{{ $plant->id }}">More info</a>
+            </article>
+        @endforeach
+    </section>
 @endsection
