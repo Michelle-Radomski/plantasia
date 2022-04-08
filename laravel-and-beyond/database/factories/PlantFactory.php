6d4fbf362fde462e48f2d7b79d41b489f968a3e2
Michelle Radomski <michi.radomski@hotmail.com>
2022-04-08 13:36:11 +0200

    Change datatype for faker

--- laravel-and-beyond/database/factories/PlantFactory.php
+++ laravel-and-beyond/database/factories/PlantFactory.php
@@ -18,8 +18,8 @@ class PlantFactory extends Factory
     {
         return [
             'user_id' => $this->faker->numberBetween(1, 10),
-            'nickname' => $this->faker->name(),
-            'official_name' => $this->faker->name(),
+            'nickname' => $this->faker->firstName(),
+            'official_name' => $this->faker->randomElement(['Epipremnum Aureum', 'Asparagus Setaceus', 'Dieffenbachia Camilla', 'Philodendron Red Emerald', 'Humata Tyermannii', 'Chamaedorea Elegans']),
             'toxic' => $this->faker->randomElement(['yes', 'no']),
             'light' => $this->faker->words(3, true),
             'temperature' => $this->faker->words(3, true),
@@ -27,7 +27,7 @@ class PlantFactory extends Factory
             'water' => $this->faker->sentence(6),
             'plant_fertilizer' => $this->faker->sentence(6),
             'soil' => $this->faker->sentence(6),
-            'misting' => $this->faker->words(3, true),
+            'misting' => $this->faker->randomElement(['yes', 'no', 'optionally']),
             'repot' => $this->faker->sentence(6),
             'air_purifying' => $this->faker->randomElement(['yes', 'no']),
         ];
